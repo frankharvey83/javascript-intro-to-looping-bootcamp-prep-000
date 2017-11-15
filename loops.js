b923c466ff846fbd8df121df6a4ec529f8d8ab96
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-15 04:47:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,11 +18,13 @@ function whileLoop(n) {
   return 'done';
 }
 
-// function doWhileLoop(array) {
-//   let
-//   do {
-//     array.pop();
-//   }
-//   while (array.length  > -1 && maybeTrue());
-//   return array;
-// }
+function doWhileLoop(array) {
+  function maybeTrue() {
+    Math.random();
+  }
+  do {
+    array.pop();
+  }
+  while (array.length  > -1 && maybeTrue());
+  return array;
+}
